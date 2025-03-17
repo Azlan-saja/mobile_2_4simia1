3ec650386403300ae1263ce179e3b8b1082e772d
Azlan-saja <azlansaja19@gmail.com>
2025-03-17 11:08:21 +0700

    p7

---
+++ lib/screen/navigation/kirim_terima_data.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class KirimTerimaData extends StatelessWidget {
+  const KirimTerimaData({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Kirim & Terima Data'),
+      ),
+    );
+  }
+}
---
+++ lib/screen/navigation/terima_data.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TerimaData extends StatelessWidget {
+  const TerimaData({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Terima Data'),
+      ),
+    );
+  }
+}
